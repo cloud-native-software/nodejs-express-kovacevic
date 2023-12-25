d39ab193b5edf25e8c2731adee19c7ff4108aee7
aleksakovacevic01 <128324791+aleksakovacevic01@users.noreply.github.com>
2023-12-25 20:31:35 +0100

    delete

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const dotenv = require('dotenv').config
+const dotenv = require('dotenv').config()
+const bodyParser = require('body-parser'); // Potrebno za parsiranje tela zahteva u POST metodu
 const client = require('./connection.js')
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+app.use(express.json(), bodyParser.json())
 client.connect()
 
 app.get('/users', (req, res) => {
@@ -27,6 +28,26 @@ app.post('/users', (req, res) => {
   })
 
 })
+
+
+
+app.delete('/users/:id', async (req, res) => {
+  const userid = req.params.id;
+
+  try {
+    const result = await client.query('DELETE FROM test WHERE id = $1', [userid]);
+    
+    if (result.rowCount === 1) {
+      res.status(200).json({ message: 'Korisnik uspešno obrisan.' });
+    } else {
+      res.status(404).json({ message: 'Korisnik nije pronađen.' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Interna server greška');
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
