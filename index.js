554030a1c2ea6234a71af7d5dea5d7db378ad9c8
aleksakovacevic01 <128324791+aleksakovacevic01@users.noreply.github.com>
2023-12-25 18:53:24 +0100

    post and db connection

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+const dotenv = require('dotenv').config
+const client = require('./connection.js')
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json())
+client.connect()
 
-app.post('/',(req,res) => {
-  res.send('Post')
+app.get('/users', (req, res) => {
+  client.query(`SELECT * FROM test`, (err, result) => {
+    if (err) {
+      res.send(err)
+    }
+    res.send(result.rows)
+
+  })
+  client.end
 })
+app.post('/users', (req, res) => {
+  const { id, firstname, lastname, location } = req.body
+  client.query(`INSERT INTO test VALUES ($1,$2,$3,$4)`, [id, firstname, lastname, location], (err, result) => {
+    if (err) {
+      res.send(err)
+    }
+    res.send(result)
+  })
 
+})
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
